962ee4cae43d07c171fc644ae14b16cdc9bf3774
oyesola-dolapo <oyesolabolamide@gmail.com>
2024-05-12 01:19:18 +0100

    item layout

--- src/components/Products/AllProducts.jsx
+++ src/components/Products/AllProducts.jsx
@@ -30,20 +30,20 @@ export default function AllProducts() {
         All Products
       </p>
       {items.length > 0 ? (
-        <div className=" flex justify-between sm:justify-start flex-wrap mt-[1rem] sm:gap-4">
+        <div className=" flex justify-between xl:justify-start flex-wrap mt-[1rem] md:px-[2rem] xl:gap-4">
           {items.map((item) => {
             return (
               <Link
                 to={`/item/${item.id}`}
-                className="flex flex-col overflow-hidden min-w-[10.5rem] max-w-[10.5rem] xl:max-w-[15rem] xl:min-w-[15rem] mb-[1rem]">
-                <div className="bg-black bg-opacity-[.1] flex items-center justify-center px-2 min-h-[15rem] max-h-[15rem] xl:max-h-[21rem] xl:min-h-[21rem] w-full">
+                className="flex flex-col overflow-hidden w-[49%] xl:w-[17rem] mb-[1rem]">
+                <div className="bg-black bg-opacity-[.1] flex items-center justify-center px-2 h-[15rem] md:h-[30rem] xl:max-h-[21rem] xl:min-h-[21rem] w-full">
                   <img
                     src={item.mainImageURL}
                     alt=""
-                    className="object-cover"
+                    className="h-[14rem] md:h-[25rem] xl:h-[20rem] object-cover"
                   />
                 </div>
-                <p className="tracking-wider my-[.6rem] text-[.8rem]">
+                <p className="tracking-wider my-[.6rem] text-[.8rem]  md:text-[1rem]">
                   {item.name}
                 </p>
                 <p className="tracking-wider text-[1rem]">{item.price} CAD</p>
--- src/components/Products/LatestProducts.jsx
+++ src/components/Products/LatestProducts.jsx
@@ -31,20 +31,20 @@ export default function LatestProducts() {
         Latest Drop
       </p>
       {items.length > 0 ? (
-        <div className=" flex justify-between sm:justify-start flex-wrap mt-[1rem] sm:gap-4">
+        <div className=" flex justify-between xl:justify-start flex-wrap mt-[1rem] md:px-[2rem]">
           {items.map((item) => {
             return (
               <Link
                 to={`/item/${item.id}`}
-                className="flex flex-col overflow-hidden min-w-[10.5rem] max-w-[10.5rem] xl:max-w-[15rem] xl:min-w-[15rem] mb-[1rem]">
-                <div className="bg-black bg-opacity-[.1] flex items-center justify-center px-2 min-h-[15rem] max-h-[15rem] xl:max-h-[21rem] xl:min-h-[21rem] w-full">
+                className="flex flex-col overflow-hidden w-[45%] xl:max-w-[15rem] xl:min-w-[15rem] mb-[1rem]">
+                <div className="bg-black bg-opacity-[.1] flex items-center justify-center px-2 h-[15rem] md:h-[30rem] xl:max-h-[21rem] xl:min-h-[21rem] w-full">
                   <img
                     src={item.mainImageURL}
                     alt=""
-                    className="object-cover"
+                    className="h-[14rem] md:h-[25rem] object-cover"
                   />
                 </div>
-                <p className="tracking-wider my-[.6rem] text-[.8rem]">
+                <p className="tracking-wider my-[.6rem] text-[.8rem] md:text-[1rem]">
                   {item.name}
                 </p>
                 <p className="tracking-wider text-[1rem]">{item.price} CAD</p>
